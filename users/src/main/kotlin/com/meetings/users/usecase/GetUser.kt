f8f9f5d0eaf3be8d812a6b1529b5bf49f021ff15
Filip Egeric <filip.egeric@gmail.com>
2021-09-07 18:30:23 +0200

    Add get user tests

--- users/src/main/kotlin/com/meetings/users/usecase/GetUser.kt
+++ users/src/main/kotlin/com/meetings/users/usecase/GetUser.kt
@@ -6,12 +6,12 @@ import com.github.michaelbull.result.Result
 import com.meetings.users.port.UsersRepository
 
 data class GetUserRequest(val id: String)
-class UserNotFound : Throwable()
+object UserNotFound : Throwable()
 typealias GetUserResponse = Result<UserDto, Throwable>
 
 internal class GetUser(private val repository: UsersRepository) {
     fun execute(request: GetUserRequest): GetUserResponse {
-        val user = repository.findOne(request.id) ?: return Err(UserNotFound())
+        val user = repository.findOne(request.id) ?: return Err(UserNotFound)
         return Ok(user.toDto())
     }
 }
---
+++ users/src/test/kotlin/com/meetings/users/GetUserTests.kt
@@ -0,0 +1,42 @@
+package com.meetings.users
+
+import com.github.michaelbull.result.unwrap
+import com.meetings.users.adapter.MemoryUsersRepository
+import com.meetings.users.usecase.CreateUserRequest
+import com.meetings.users.usecase.GetUserRequest
+import com.meetings.users.usecase.UserNotFound
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class GetUserTests {
+    private lateinit var usersFacade: UsersFacade
+
+    @BeforeEach
+    fun init() {
+        usersFacade = UsersFacade(MemoryUsersRepository())
+    }
+
+    @Test
+    fun `returns proper error if user is not previously created`() {
+        val (userDto, error) = usersFacade.getUser(GetUserRequest("non-existing"))
+        assertNull(userDto)
+        assertEquals(UserNotFound, error)
+    }
+
+    @Test
+    fun `returns previously created user`() {
+        createUser("an-id", "example@mail.com")
+
+        val result = usersFacade.getUser(GetUserRequest("an-id"))
+        val userDto = result.unwrap()
+
+        assertEquals(userDto.id, "an-id")
+        assertEquals(userDto.email, "example@mail.com")
+    }
+
+    private fun createUser(id: String, email: String) {
+        usersFacade.createUser(CreateUserRequest(id, email))
+    }
+}
